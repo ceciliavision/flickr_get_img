d165d94da557487ed9c11deb4f8154ddda86318e
ceciliavision <cecilia77@berkeley.edu>
2017-05-04 21:29:05 -0700

    add comments and README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # flickr_get_img
 Bash script to automatically download photos given photo query ID (User ID not required)
+
+Originally written to extract Flickr photos in dataset Dubrovnik6K (http://www.cs.cornell.edu/projects/p2f/), a subset of SFM data.
+
+Data from the original webiste doesn't include the images but only Flickr user ID and photo ID. The script follows the instruction of the Dubrovnik6K dataset usage (available in the link above), and automatically downloads all images with known focal length.
+
+# Usage
+The only argument is the list file. An example list file is included in this directory. It's also the real list file of the original Dubrovnik6K dataset. Simply type in the shell:
+bash extract_img.sh list.db.txt
--- extract_img.sh
+++ extract_img.sh
@@ -4,18 +4,18 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	extr="http://www.flickr.com/photo_zoom.gne?id=";
 	ext=".jpg";
 	# print length($a);
-	if [[ -z "${a[2]// }" ]]; then # filter out files with focal length
-		nd=(${a[0]//// });
+	if [[ -z "${a[2]// }" ]]; then # filter out files with focal length, if loop can be comment out if no filtering is needed
+		nd=(${a[0]//// }); # separate /
 		# echo "$a[1]";
-		id_ext=(${a[0]//_/ });
-		id=(${id_ext[-1]//./ });
+		id_ext=(${a[0]//_/ }); # separate the photo id
+		id=(${id_ext[-1]//./ }); # separate the extension
 		name=${id[0]};
-		fullname=$extr$name;
+		fullname=$extr$name; # concat to get the full webpage link
 		# echo "$fullname";
-		wget "$fullname";
+		wget "$fullname"; # to inspect the webpage source in order to search (next line)
 		wget -O ./images/$name.jpg `cat photo_zoom.gne\?id="$name" | grep -B 1 "Download the Large 1024 size of this photo" | head -n 1 | sed -e 's/.*="//' -e 's/">//'`
 	fi
 
 done < "$1"
 
-rm photo*
+rm photo* # clean up, remove all saved webpage sources
